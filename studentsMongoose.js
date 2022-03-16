f38ad8aa76451abd41c2a34ee694d15b19d53be2
Saikrishnakmnr <saikrishna4234@gmail.com>
2022-03-16 15:49:14 +0530

    package and studentsmangoose updated

--- studentsMongoose.js
+++ studentsMongoose.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
   mongoose.connect("mongodb://localhost/SCHOOL").then(()=>{
-    console.log('connected to monodb')
+    console.log('connected to mongodb')
 })
 .catch(err=>{
     console.log('inserting not posiible')
